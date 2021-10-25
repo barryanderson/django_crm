947bf6a8f1120159bcab9181bd1925468bc99f7b
Barry Anderson <barryanderson@users.noreply.github.com>
2021-10-25 16:36:55 +0100

    Update urls to names

--- leads/urls.py
+++ leads/urls.py
@@ -2,10 +2,12 @@ from django.urls import path
 
 from leads.views import lead_detail, lead_list, lead_create, lead_update, lead_delete
 
+app_name = "leads"
+
 urlpatterns = [
-    path('', lead_list),
-    path('create/', lead_create),
-    path('<int:pk>/update/', lead_update),
-    path('<int:pk>/delete/', lead_delete),
-    path('<int:pk>', lead_detail)
+    path('', lead_list, name='lead-list'),
+    path('<int:pk>', lead_detail, name='lead-detail'),
+    path('<int:pk>/update/', lead_update, name='lead-update'),
+    path('<int:pk>/delete/', lead_delete, name='lead-delete'),
+    path('create/', lead_create, name='lead-create'),
 ]
